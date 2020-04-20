b1dd87af8be7537b1cd5b5be5773bd5f39be04a9
gk42111 <56386645+gk42111@users.noreply.github.com>
2020-04-20 23:31:11 +0200

    Add files via upload

---
+++ Rectangle.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include "Rectangle.h"
+Rectangle::Rectangle(int X, int Y, double Side) :Polygon(X, Y)
+{
+	this->Side = Side;
+	NumbersOfSides = 4;
+	Sides = new double[4];
+	for (int i = 0; i < 4; i++)
+	{
+		Sides[i] = Side;
+	}
+	obwod = Perimeter();
+	pole = Area();
+}
+Rectangle::~Rectangle()
+{
+}
+double Rectangle::Area()
+{
+	return Side * Side;
+}
+std::ostream & operator<<(std::ostream & wyjscie, const Rectangle & s)
+{
+	return wyjscie << "To jest kwadrat\n Po³o¿ony:\t" << s.X << " " << s.Y << std::endl << "Obwód\t" <<  s.obwod << std::endl << "Pole\t" << s.pole << std::endl;
+}
---
+++ Rectangle.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "Polygon.h"
+
+class Rectangle : public Polygon
+{
+private:
+	double Side;
+	double obwod;
+	double pole = 2;
+public:
+	Rectangle(int X, int Y, double Side);
+	~Rectangle();
+	double Area();
+	friend std::ostream & operator<< (std::ostream &wyjscie, const Rectangle &s);
+
+};
+
